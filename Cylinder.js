a35c4d823a64349fc36a4635b1e9403e89d6e1b6
Kaye Suarez <suarezka@mail.gvsu.edu>
2017-03-09 12:36:34 -0500

    Made lots of pineapples

--- Cylinder.js
+++ Cylinder.js
@@ -1,178 +1,91 @@
 /**
- * Created by Kaye on 2/13/17.
  * Created by Hans Dulimarta on 2/1/17.
  */
-
+// Create a cylinder whose Z-axis as its axis of symmetry, base at Z=-h/2, top at Z=+h/2
 class Cylinder {
-    /**
-     * Create a 3D cylinder with tip at the Z+ axis and base on the XY plane
-     * @param {Object} gl      the current WebGL context
-     * @param {Number} radiusT  radius of the top
-     * @param {Number} radiusB  radius of the bottom
-     * @param {Number} height  height of the cone
-     * @param {Number} subDiv  number of radial subdivision of the cone base
-     * @param {Number} stacks  number of vertical stacks of cone
-     * @param {vec3}   col1    color #1 to use
-     * @param {vec3}   col2    color #2 to use
-     */
-    constructor(gl, radiusT, radiusB, height, subDiv, stacks, col1, col2) {
-
-        /* if colors are undefined, generate random colors */
-        if (typeof col1 === "undefined") col1 = vec3.fromValues(Math.random(), Math.random(), Math.random());
-        if (typeof col2 === "undefined") col2 = vec3.fromValues(Math.random(), Math.random(), Math.random());
-        let randColor = vec3.create();
+    /* subDiv: number of subdivisions for the circle/cone base */
+    constructor (gl, topRadius, botRadius, height, subDiv, col1, col2) {
+        if (typeof col1 === "undefined") col1 = vec3.fromValues(0xff/255, 0x59/255, 0x59/255);
+        if (typeof col2 === "undefined") col2 = vec3.fromValues(0xFF/255, 0xC5/255, 0x6C/255);
         let vertices = [];
+        let randColor = vec3.create();
         this.vbuff = gl.createBuffer();
 
-        //VERTICAL INTERVAL OF STACKS
-        let div = height / stacks;
-
-        /* Instead of allocating two separate JS arrays (one for position and one for color),
-         in the following loop we pack both position and color
-         so each tuple (x,y,z,r,g,b) describes the properties of a vertex
-         */
-        vertices.push(0, 0, height);
-        /* tip of cone */
-        vec3.lerp(randColor, col1, col2, Math.random());
-        /* linear interpolation between two colors */
+        /* create the top points */
+        vertices.push(0,0,height/2);
+        vec3.lerp (randColor, col1, col2, Math.random()); /* linear interpolation between two colors */
         vertices.push(randColor[0], randColor[1], randColor[2]);
-
-        //Top of cyclinder
         for (let k = 0; k < subDiv; k++) {
             let angle = k * 2 * Math.PI / subDiv;
-            let x = radiusT * Math.cos(angle);
-            let y = radiusT * Math.sin(angle);
-
-            /* the first three floats are 3D (x,y,z) position */
-            vertices.push(x, y, height);
-            /* perimeter of base */
-            vec3.lerp(randColor, col1, col2, Math.random());
-            /* linear interpolation between two colors */
-            /* the next three floats are RGB */
+            let x = topRadius * Math.cos (angle);
+            let y = topRadius * Math.sin (angle);
+            vertices.push (x, y, height/2); /* perimeter */
+            vec3.lerp (randColor, col1, col2, Math.random()); /* linear interpolation between two colors */
             vertices.push(randColor[0], randColor[1], randColor[2]);
         }
-
-        //More than 1 vertical subdivision
-        if (stacks > 1) {
-            let r = 0;
-            let h = height;
-            for (let i = 1; i < stacks; i++) {
-                let n = (Math.abs(radiusT - radiusB));
-                r = radiusT + (i * (n / stacks));
-                h = height - (i * (height / stacks));
-
-                for (let k = 0; k < subDiv; k++) {
-                    let angle = k * 2 * Math.PI / subDiv;
-                    let x = r * Math.cos(angle);
-                    let y = r * Math.sin(angle);
-
-                    vertices.push(x, y, h);
-                    vec3.lerp(randColor, col1, col2, Math.random());
-
-                    vertices.push(randColor[0], randColor[1], randColor[2]);
-                }
-            }
-        }
-
-        //Repeat steps for bottom of cylinder
+        /* create the bottom points */
+        vertices.push(0,0, -height/2);
+        vec3.lerp (randColor, col1, col2, Math.random()); /* linear interpolation between two colors */
+        vertices.push(randColor[0], randColor[1], randColor[2]);
         for (let k = 0; k < subDiv; k++) {
             let angle = k * 2 * Math.PI / subDiv;
-            let x = radiusB * Math.cos(angle);
-            let y = radiusB * Math.sin(angle);
-
-            /* the first three floats are 3D (x,y,z) position */
-            vertices.push(x, y, 0);
-            /* perimeter of base */
-            vec3.lerp(randColor, col1, col2, Math.random());
-            /* linear interpolation between two colors */
-            /* the next three floats are RGB */
+            let x = botRadius * Math.cos (angle);
+            let y = botRadius * Math.sin (angle);
+            vertices.push (x, y, -height/2); /* perimeter */
+            vec3.lerp (randColor, col1, col2, Math.random()); /* linear interpolation between two colors */
             vertices.push(randColor[0], randColor[1], randColor[2]);
         }
-
-        vertices.push(0, 0, 0);
-        /* center of base */
-        vec3.lerp(randColor, col1, col2, Math.random());
-        /* linear interpolation between two colors */
-        vertices.push(randColor[0], randColor[1], randColor[2]);
-
-
-        /* copy the (x,y,z,r,g,b) sixtuplet into GPU buffer */
         gl.bindBuffer(gl.ARRAY_BUFFER, this.vbuff);
         gl.bufferData(gl.ARRAY_BUFFER, Float32Array.from(vertices), gl.STATIC_DRAW);
 
-        console.log(vertices);
-
-        // Generate index order for top of cylinder
+        // Generate index for top circle
         let topIndex = [];
         topIndex.push(0);
         for (let k = 1; k <= subDiv; k++)
             topIndex.push(k);
         topIndex.push(1);
-        this.topIdxBuff = gl.createBuffer();
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.topIdxBuff);
+        let topIdxBuff = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, topIdxBuff);
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, Uint8Array.from(topIndex), gl.STATIC_DRAW);
 
-        // Generate index order for bottom of cylinder
+        // Generate index for bottom of cone
         let botIndex = [];
-        let n = (stacks * subDiv) + 1;
-        botIndex.push(n + subDiv);
-        for (let k = n + (subDiv - 1); k >= n; k--)
+        botIndex.push(subDiv + 1);
+        for (let k = 2*subDiv - 1; k >= subDiv + 2; k--)
             botIndex.push(k);
-        botIndex.push(n + (subDiv - 1));
-
-        this.botIdxBuff = gl.createBuffer();
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.botIdxBuff);
+        botIndex.push(2*subDiv - 1);
+        let botIdxBuff = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, botIdxBuff);
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, Uint8Array.from(botIndex), gl.STATIC_DRAW);
 
-        //Generate index for cylinder vertices
-        let vertIndex = [];
-        for (let k = 1; k < (stacks * subDiv) + 1; k++) {
-            vertIndex.push(k);
-            vertIndex.push(k + subDiv);
-
-            if (k % subDiv == 0) {
-                vertIndex.push(k - (subDiv - 1));
-                vertIndex.push(k + 1);
-            }
+        // Generate index for the side
+        let sideIndex = [];
+        for (let k = 1; k <= subDiv; k++) {
+            sideIndex.push (k, k + subDiv + 1);
         }
+        sideIndex.push (1, subDiv + 2);
+        let sideIdxBuf = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, sideIdxBuf);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, Uint8Array.from(sideIndex), gl.STATIC_DRAW);
 
-        console.log(topIndex);
-        console.log(botIndex);
-        console.log(vertIndex);
-
-        this.vertIdxBuff = gl.createBuffer();
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.vertIdxBuff);
-        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, Uint8Array.from(vertIndex), gl.STATIC_DRAW);
-
-
-        /* Put the indices as an array of objects. Each object has three attributes:
-         primitive, buffer, and numPoints */
-        this.indices = [{"primitive": gl.TRIANGLE_FAN, "buffer": this.topIdxBuff, "numPoints": topIndex.length},
-                        {"primitive": gl.TRIANGLE_FAN, "buffer": this.botIdxBuff, "numPoints": botIndex.length},
-                        {"primitive": gl.TRIANGLE_STRIP, "buffer": this.vertIdxBuff, "numPoints": vertIndex.length}];
+        /* Put the indices as an array of objects */
+        this.indices = [
+            {"primitive": gl.TRIANGLE_FAN, "buffer": topIdxBuff, "numPoints": topIndex.length},
+            {"primitive": gl.TRIANGLE_FAN, "buffer": botIdxBuff, "numPoints": botIndex.length},
+            {"primitive": gl.TRIANGLE_STRIP, "buffer": sideIdxBuf, "numPoints": sideIndex.length}
+        ];
     }
 
-    /**
-     * Draw the object
-     * @param {Number} vertexAttr a handle to a vec3 attribute in the vertex shader for vertex xyz-position
-     * @param {Number} colorAttr  a handle to a vec3 attribute in the vertex shader for vertex rgb-color
-     * @param {Number} modelUniform a handle to a mat4 uniform in the shader for the coordinate frame of the model
-     * @param {mat4} coordFrame a JS mat4 variable that holds the actual coordinate frame of the object
-     */
-    draw(vertexAttr, colorAttr, modelUniform, coordFrame) {
-        /* copy the coordinate frame matrix to the uniform memory in shader */
+    getData() {
+        return {"vertex" : this.vbuff,
+            "index"  : this.indices};   /* this field is actually an array */
+    }
+
+    draw (vertexAttr, colorAttr, modelUniform, coordFrame) {
         gl.uniformMatrix4fv(modelUniform, false, coordFrame);
-
-        /* binder the (vertex+color) buffer */
         gl.bindBuffer(gl.ARRAY_BUFFER, this.vbuff);
-
-        /* with the "packed layout"  (x,y,z,r,g,b),
-         the stride distance between one group to the next is 24 bytes */
         gl.vertexAttribPointer(vertexAttr, 3, gl.FLOAT, false, 24, 0);
-        /* (x,y,z) begins at offset 0 */
         gl.vertexAttribPointer(colorAttr, 3, gl.FLOAT, false, 24, 12);
-        /* (r,g,b) begins at offset 12 */
-
         for (let k = 0; k < this.indices.length; k++) {
             let obj = this.indices[k];
             gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, obj.buffer);
--- Pineapple.js
+++ Pineapple.js
@@ -15,11 +15,11 @@ class Pineapple {
         let crownColor2 = vec3.fromValues(0.0, 0.392157, 0.0);
 
 
-        this.base = new Cylinder(gl, 0.30, 0.25, 0.4, 16, 1, baseColor, baseColor2);
-        this.top = new Cylinder(gl, 0.30, 0.25, 0.4, 16, 1, baseColor, baseColor2);
-        this.crown = new Cone(gl, 0.2, 0.4, 15, 1, crownColor, crownColor2);
-        this.c2 = new Cone(gl, 0.2, 0.4, 15, 1, crownColor, crownColor2);
-        this.c3 = new Cone(gl, 0.2, 0.4, 15, 1, crownColor, crownColor2);
+        this.base = new Cylinder(gl, 0.20, 0.15, 0.2, 16, baseColor, baseColor2);
+        this.top = new Cylinder(gl, 0.20, 0.15, 0.2, 16, baseColor, baseColor2);
+        this.c1 = new Cone(gl, 0.12, 0.2, 15, 1, crownColor, crownColor2);
+        this.c2 = new Cone(gl, 0.11, 0.2, 15, 1, crownColor, crownColor2);
+        this.c3 = new Cone(gl, 0.1, 0.2, 15, 1, crownColor, crownColor2);
         this.sideCones = [
             new Cone(gl, 0.1, 0.4, 15, 1),
             new Cone(gl, 0.1, 0.4, 15, 1),
@@ -37,7 +37,7 @@ class Pineapple {
         //Repositioning for Base and Top
         this.baseTransform = mat4.create();
 
-        let moveUp = vec3.fromValues (0, 0, 0.8);
+        let moveUp = vec3.fromValues (0, 0, 0.2);
         this.topTransform = mat4.create();
         mat4.rotateY(this.topTransform, this.topTransform, Math.PI);
         let moveUpTop = mat4.fromTranslation(mat4.create(), moveUp);
@@ -45,22 +45,22 @@ class Pineapple {
         mat4.multiply (this.topTransform, moveUpTop, this.topTransform);
 
 
-        //Repositioning Crown of Cones
-        this.crownTransform = mat4.create();
-        mat4.rotateY(this.crownTransform, this.crownTransform, Math.PI);
+        //Repositioning C1 of Cones
+        this.c1Transform = mat4.create();
+        mat4.rotateY(this.c1Transform, this.c1Transform, Math.PI);
 
-        let moveUp2 = vec3.fromValues(0, 0, 0.95);
-        let moveUpCrown = mat4.fromTranslation(mat4.create(), moveUp2);
+        let moveUp2 = vec3.fromValues(0, 0, 0.4);
+        let moveUpC1 = mat4.fromTranslation(mat4.create(), moveUp2);
 
         //crownTransform = moveUpCrown * crownTransform
-        mat4.multiply (this.crownTransform, moveUpCrown, this.crownTransform);
+        mat4.multiply (this.c1Transform, moveUpC1, this.c1Transform);
 
 
         //C2
         this.c2Transform = mat4.create();
         mat4.rotateY(this.c2Transform, this.c2Transform, Math.PI);
 
-        let moveUp3 = vec3.fromValues(0, 0, 1.1);
+        let moveUp3 = vec3.fromValues(0, 0, 0.5);
         let moveUpC2 = mat4.fromTranslation(mat4.create(), moveUp3);
 
         //crownTransform = moveUpCrown * crownTransform
@@ -71,7 +71,7 @@ class Pineapple {
         this.c3Transform = mat4.create();
         mat4.rotateY(this.c3Transform, this.c3Transform, Math.PI);
 
-        let moveUp4 = vec3.fromValues(0, 0, 1.25);
+        let moveUp4 = vec3.fromValues(0, 0, 0.6);
         let moveUpC3 = mat4.fromTranslation(mat4.create(), moveUp4);
 
         //crownTransform = moveUpCrown * crownTransform
@@ -87,18 +87,18 @@ class Pineapple {
         mat4.mul (this.tmp, coordFrame, this.baseTransform);
         this.base.draw(vertexAttr, colorAttr, modelUniform, this.tmp);
 
-
+/*
         for (let k = 0; k < 3; k++) {
             mat4.mul (this.tmp, coordFrame, this.sideConesTransfrom);
             this.sideCones[k].draw(vertexAttr, colorAttr, modelUniform, this.tmp);
 
         }
-
+*/
         mat4.mul (this.tmp, coordFrame, this.topTransform);
         this.top.draw(vertexAttr, colorAttr, modelUniform, this.tmp);
 
-        mat4.mul (this.tmp, coordFrame, this.crownTransform);
-        this.crown.draw(vertexAttr, colorAttr, modelUniform, this.tmp);
+        mat4.mul (this.tmp, coordFrame, this.c1Transform);
+        this.c1.draw(vertexAttr, colorAttr, modelUniform, this.tmp);
 
         mat4.mul (this.tmp, coordFrame, this.c2Transform);
         this.c2.draw(vertexAttr, colorAttr, modelUniform, this.tmp);
